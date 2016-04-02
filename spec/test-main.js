36829e2edcf7b2933e480054d1939068c6fcf749
Jesse White <jesse@opennms.org>
2016-04-02 11:30:46 -0400

    Add a basic test suite.

---
+++ spec/test-main.js
@@ -0,0 +1,22 @@
+import prunk from 'prunk';
+import {jsdom} from 'jsdom';
+import chai from 'chai';
+
+// Mock Grafana modules that are not available outside of the core project
+// Required for loading module.js
+prunk.mock('./css/query-editor.css!', 'no css, dude.');
+prunk.mock('app/plugins/sdk', {
+    QueryCtrl: null
+});
+
+// Setup jsdom
+// Required for loading angularjs
+global.document = jsdom('<html><head><script></script></head><body></body></html>');
+global.window = global.document.parentWindow;
+global.navigator = window.navigator = {};
+global.Node = window.Node;
+
+// Setup Chai
+chai.should();
+global.assert = chai.assert;
+global.expect = chai.expect;
