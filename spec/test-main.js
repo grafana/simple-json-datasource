bd1dfa0c7e47e17a887b678e241a9bd1c7572806
Konstantin Koniev <kostiantyn_koniev@symbotic.com>
2017-04-21 16:51:31 +0300

    Update packages and fix tests.

--- spec/test-main.js
+++ spec/test-main.js
@@ -13,8 +13,6 @@ prunk.mock('app/plugins/sdk', {
 // Required for loading angularjs
 global.document = jsdom('<html><head><script></script></head><body></body></html>');
 global.window = global.document.parentWindow;
-global.navigator = window.navigator = {};
-global.Node = window.Node;
 
 // Setup Chai
 chai.should();
