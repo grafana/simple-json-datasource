1341177126392b5b56bb25bda627fc5ef2c3b6af
Torkel Ödegaard <torkel@grafana.com>
2018-05-16 13:01:10 +0200

    updated deps

--- vendor/github.com/hashicorp/go-hclog/logger_test.go
+++ vendor/github.com/hashicorp/go-hclog/logger_test.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -107,7 +108,7 @@ func TestLogger(t *testing.T) {
 		rest := str[dataIdx+1:]
 
 		// This test will break if you move this around, it's line dependent, just fyi
-		assert.Equal(t, "[INFO ] go-hclog/logger_test.go:100: test: this is test: who=programmer why=\"testing is fun\"\n", rest)
+		assert.Equal(t, "[INFO ] go-hclog/logger_test.go:101: test: this is test: who=programmer why=\"testing is fun\"\n", rest)
 	})
 
 	t.Run("prefixes the name", func(t *testing.T) {
@@ -133,12 +134,108 @@ func TestLogger(t *testing.T) {
 		rest = str[dataIdx+1:]
 		assert.Equal(t, "[INFO ] sublogger: this is test\n", rest)
 	})
+
+	t.Run("use a different time format", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		logger := New(&LoggerOptions{
+			Name:       "test",
+			Output:     &buf,
+			TimeFormat: time.Kitchen,
+		})
+
+		logger.Info("this is test", "who", "programmer", "why", "testing is fun")
+
+		str := buf.String()
+
+		dataIdx := strings.IndexByte(str, ' ')
+
+		assert.Equal(t, str[:dataIdx], time.Now().Format(time.Kitchen))
+	})
+
+	t.Run("use with", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		rootLogger := New(&LoggerOptions{
+			Name:   "with_test",
+			Output: &buf,
+		})
+
+		// Build the root logger in two steps, which triggers a slice capacity increase
+		// and is part of the test for inadvertant slice aliasing.
+		rootLogger = rootLogger.With("a", 1, "b", 2)
+		rootLogger = rootLogger.With("c", 3)
+
+		// Derive two new loggers which should be completely independent
+		derived1 := rootLogger.With("cat", 30)
+		derived2 := rootLogger.With("dog", 40)
+
+		derived1.Info("test1")
+		output := buf.String()
+		dataIdx := strings.IndexByte(output, ' ')
+		assert.Equal(t, "[INFO ] with_test: test1: a=1 b=2 c=3 cat=30\n", output[dataIdx+1:])
+
+		buf.Reset()
+
+		derived2.Info("test2")
+		output = buf.String()
+		dataIdx = strings.IndexByte(output, ' ')
+		assert.Equal(t, "[INFO ] with_test: test2: a=1 b=2 c=3 dog=40\n", output[dataIdx+1:])
+	})
+
+	t.Run("use with and log", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		rootLogger := New(&LoggerOptions{
+			Name:   "with_test",
+			Output: &buf,
+		})
+
+		// Build the root logger in two steps, which triggers a slice capacity increase
+		// and is part of the test for inadvertant slice aliasing.
+		rootLogger = rootLogger.With("a", 1, "b", 2)
+		rootLogger = rootLogger.With("c", 3)
+
+		// Derive another logger which should be completely independent of rootLogger
+		derived := rootLogger.With("cat", 30)
+
+		rootLogger.Info("root_test", "bird", 10)
+		output := buf.String()
+		dataIdx := strings.IndexByte(output, ' ')
+		assert.Equal(t, "[INFO ] with_test: root_test: a=1 b=2 c=3 bird=10\n", output[dataIdx+1:])
+
+		buf.Reset()
+
+		derived.Info("derived_test")
+		output = buf.String()
+		dataIdx = strings.IndexByte(output, ' ')
+		assert.Equal(t, "[INFO ] with_test: derived_test: a=1 b=2 c=3 cat=30\n", output[dataIdx+1:])
+	})
+
+	t.Run("supports Printf style expansions when requested", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		logger := New(&LoggerOptions{
+			Name:   "test",
+			Output: &buf,
+		})
+
+		logger.Info("this is test", "production", Fmt("%d beans/day", 12))
+
+		str := buf.String()
+
+		dataIdx := strings.IndexByte(str, ' ')
+
+		// ts := str[:dataIdx]
+		rest := str[dataIdx+1:]
+
+		assert.Equal(t, "[INFO ] test: this is test: production=\"12 beans/day\"\n", rest)
+	})
 }
 
 func TestLogger_JSON(t *testing.T) {
 	t.Run("json formatting", func(t *testing.T) {
 		var buf bytes.Buffer
-
 		logger := New(&LoggerOptions{
 			Name:       "test",
 			Output:     &buf,
@@ -158,6 +255,32 @@ func TestLogger_JSON(t *testing.T) {
 		assert.Equal(t, "programmer", raw["who"])
 		assert.Equal(t, "testing is fun", raw["why"])
 	})
+
+	t.Run("json formatting with", func(t *testing.T) {
+		var buf bytes.Buffer
+		logger := New(&LoggerOptions{
+			Name:       "test",
+			Output:     &buf,
+			JSONFormat: true,
+		})
+		logger = logger.With("cat", "in the hat", "dog", 42)
+
+		logger.Info("this is test", "who", "programmer", "why", "testing is fun")
+
+		b := buf.Bytes()
+
+		var raw map[string]interface{}
+		if err := json.Unmarshal(b, &raw); err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, "this is test", raw["@message"])
+		assert.Equal(t, "programmer", raw["who"])
+		assert.Equal(t, "testing is fun", raw["why"])
+		assert.Equal(t, "in the hat", raw["cat"])
+		assert.Equal(t, float64(42), raw["dog"])
+	})
+
 	t.Run("json formatting error type", func(t *testing.T) {
 		var buf bytes.Buffer
 
@@ -181,6 +304,7 @@ func TestLogger_JSON(t *testing.T) {
 		assert.Equal(t, "programmer", raw["who"])
 		assert.Equal(t, errMsg.Error(), raw["err"])
 	})
+
 	t.Run("json formatting custom error type json marshaler", func(t *testing.T) {
 		var buf bytes.Buffer
 
@@ -213,6 +337,7 @@ func TestLogger_JSON(t *testing.T) {
 		assert.Equal(t, "programmer", raw["who"])
 		assert.Equal(t, expectedMsg, raw["err"])
 	})
+
 	t.Run("json formatting custom error type text marshaler", func(t *testing.T) {
 		var buf bytes.Buffer
 
@@ -241,6 +366,28 @@ func TestLogger_JSON(t *testing.T) {
 		assert.Equal(t, "this is test", raw["@message"])
 		assert.Equal(t, "programmer", raw["who"])
 		assert.Equal(t, expectedMsg, raw["err"])
+	})
+
+	t.Run("supports Printf style expansions when requested", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		logger := New(&LoggerOptions{
+			Name:       "test",
+			Output:     &buf,
+			JSONFormat: true,
+		})
+
+		logger.Info("this is test", "production", Fmt("%d beans/day", 12))
+
+		b := buf.Bytes()
+
+		var raw map[string]interface{}
+		if err := json.Unmarshal(b, &raw); err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, "this is test", raw["@message"])
+		assert.Equal(t, "12 beans/day", raw["production"])
 	})
 }
 
